02643d1efe63542ce8fd6add124ec04213cf3b55
xhu-zfx <756867768@qq.com>
2022-08-22 10:37:05 +0800

    28 commit

---
+++ src/leetcode/editor/cn/28-实现 strStr().java
@@ -0,0 +1,57 @@
+//实现 strStr() 函数。 
+//
+// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如
+//果不存在，则返回 -1 。 
+//
+// 说明： 
+//
+// 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。 
+//
+// 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：haystack = "hello", needle = "ll"
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：haystack = "aaaaa", needle = "bba"
+//输出：-1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= haystack.length, needle.length <= 10⁴ 
+// haystack 和 needle 仅由小写英文字符组成 
+// 
+//
+// Related Topics 双指针 字符串 字符串匹配 👍 1559 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle=="") return 0;
+        if (haystack.equals(needle)) return 0;
+        int m=haystack.length(),n=needle.length();
+
+        for (int i=0;i<=m-n;i++){
+            int a=i,b=0;
+            while (b<n&&haystack.charAt(a)==needle.charAt(b)){
+                a++;b++;
+            }
+            if (b==n) return i;
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
