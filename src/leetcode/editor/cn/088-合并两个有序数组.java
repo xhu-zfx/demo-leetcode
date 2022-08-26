fcf11ec6b45fae384151ae90949470bc2f8d3b8d
xhu-zfx <756867768@qq.com>
2022-08-26 10:27:05 +0800

    2022-8-26 commit

--- src/leetcode/editor/cn/088-合并两个有序数组.java
+++ src/leetcode/editor/cn/088-合并两个有序数组.java
@@ -54,10 +54,15 @@
 // Related Topics 数组 双指针 排序 👍 1543 👎 0
 
 
+import java.util.Arrays;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        for (int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
+        }
+        Arrays.sort(nums1);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
